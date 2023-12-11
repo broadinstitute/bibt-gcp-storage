94696f00bbad9b9651872fd002c5f2e53162325b
Matthew OBrien <mobrien@broadinstitute.org>
2023-12-11 17:51:46 -0500

    client config

--- bibt/gcp/storage/classes.py
+++ bibt/gcp/storage/classes.py
@@ -1,10 +1,12 @@
 import logging
 
+from google.cloud import storage
+
 _LOGGER = logging.getLogger(__name__)
 
 
 class Client:
     """ """
 
-    def __init__(self):
-        pass
+    def __init__(self, credentials=None):
+        self._client = storage.Client(credentials=credentials)
